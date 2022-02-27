55bf406e9246603e6efb4b669d3df225d1edab40
liezsm <liez.dev@gmail.com>
2022-02-27 16:19:53 +0800

    Added appv9

---
+++ src/Appv9.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import Form from './practices/Forms/Form';
+// import FormPractice from './practices/Forms/FormPractice';
+import FormContainer from './practices/Component-Container-Architecture/Form/FormContainer'
+
+function Appv9() {
+  return (
+    <div>
+        {/* <FormPractice /> */}
+        <FormContainer />
+    </div>
+  )
+}
+export default Appv9;
