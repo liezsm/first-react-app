bfaedc4aac69450738569b3ed42b40eefa707910
liezsm <liez.dev@gmail.com>
2022-02-27 16:00:55 +0800

    Added Appv2

---
+++ src/Appv2.js
@@ -0,0 +1,19 @@
+import Header from "./practices/fromScratch/Header";
+import Main from "./practices/fromScratch/Main";
+import Footer from "./practices/fromScratch/Footer";
+// import './practices/practice.css';
+
+
+function Appv2() {
+    return (
+        <>
+        < Header />
+        <Main />
+        <Footer />
+
+        </>
+    )
+}
+
+export default Appv2;
+
