552825c7bfbeba53bc3300da996b145ae3f68246
liezsm <liez.dev@gmail.com>
2022-02-27 16:20:22 +0800

    Added appv10

---
+++ src/Appv10.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Capstone/Header/Header'
+import MemeGenerator from './Capstone/Meme/MemeGenerator'
+
+function Appv10() {
+    return (
+        <div>
+           
+            <Header />
+            <MemeGenerator />
+        </div>
+    )
+}
+
+export default Appv10
