c852ae689f1040a8b72bf6913fb14d3ab4244f37
liezsm <liez.dev@gmail.com>
2022-02-27 11:15:58 +0800

    Modified/ deleted
    modified the files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,99 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import './index.css';
+import './App.css'
+// import App from './App';
+// import Appv2 from './Appv2';
+// import Appv3 from './Appv3';
+// import Appv4 from './Appv4';
+// import Appv5 from './Appv5';
+// import Appv6 from './Appv6';
 import reportWebVitals from './reportWebVitals';
+// import MouseEvent from './practices/MouseEvent/MouseEvent';
+// import PracticeState from './practices/Changing State/PracticeState'
+// import UseState from './practices/WDS-useState/UseState'
+// import Appv8 from './Appv8'
+// import Appv9 from './Appv9'
+import Appv10 from './Appv10'
 
+
+// -practices
+// import Practice1 from './practices/Practice1';
+// import Practice2 from './practices/Practice2';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+// ReactDOM.render(
+//   <React.StrictMode>
+//     < App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// exp for the build from scratch challenge
+// ReactDOM.render(
+//   <React.StrictMode>
+//     < Appv2 />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// exp for the build from scratch challenge for TODO app
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <Appv2 /> */}
+    {/* < Appv3 /> */}
+    {/* <MouseEvent /> */}
+  {/* <PracticeState /> */}
+  {/* <Appv6 /> */}
+  {/* <UseState /> */}
+  {/* < Appv8 /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
+// exp for the build from scratch challenge for JOKE app
+// ReactDOM.render(
+//   <React.StrictMode>
+//     < Appv4 />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// exp for the build from scratch challenge for PRODUCT app
+// ReactDOM.render(
+//   <React.StrictMode>
+//     < Appv5 />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+
+// exp for the build from scratch challenge for PRODUCT app
+// ReactDOM.render(
+//   <React.StrictMode>
+//     < Appv9 />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+
+// exp for the build from scratch challenge for PRODUCT app
+ReactDOM.render(
+  <React.StrictMode>
+    < Appv10 />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
